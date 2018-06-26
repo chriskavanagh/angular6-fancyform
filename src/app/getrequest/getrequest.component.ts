9534fc674daa4aa72f6686cb100064e62786f130
Chris Kavanagh <ckava3@gmail.com>
2018-06-26 00:42:58 -0400

    Post interface

--- src/app/getrequest/getrequest.component.ts
+++ src/app/getrequest/getrequest.component.ts
@@ -1,8 +1,9 @@
 import { Observable, of } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 // import { from } from 'rxjs';
+import { Post } from './../post'; // used in post request
 
 
 @Component({
@@ -14,12 +15,13 @@ export class GetrequestComponent implements OnInit {
 
   repos: Observable<any[]>;
   path: string = 'https://jsonplaceholder.typicode.com/posts';
+  temp: boolean = true;
   
-  
+  // ex Observable "of" with a delay of 5 seconds
   cities$: Observable<any[]> = of([
-              {name: 'Los Angeles', population: '3.9 million', elevation: '233′'},
-              {name: 'New York', population: '8,4 million', elevation: '33′'},
-              {name: 'Chicago', population: '2.7 million', elevation: '594′'},
+                  {name: 'Los Angeles', population: '3.9 million', elevation: '233′'},
+                  {name: 'New York', population: '8,4 million', elevation: '33′'},
+                  {name: 'Chicago', population: '2.7 million', elevation: '594′'},
   ])
   .pipe(delay(5000));
 
@@ -28,15 +30,17 @@ export class GetrequestComponent implements OnInit {
   
   ngOnInit() {
       // get request    
-      this.repos = this.http.get<any[]>(this.path) // async in template, no need to subscribe.
+      this.repos = this.http.get<any[]>(this.path); // async in template, no need to subscribe.
       // .subscribe(data => this.repos = data)
       
       // post request
-      const params = new HttpParams()
-                  .set("title", "Foo Title")
-                  .set("body", "I am body")
-                  .set("userId","1");
-      const req = this.http.post(this.path, {params})
+      const data: Post = {
+        title: "Foo Title",
+        body: "I am body",
+        userId: 1
+      }
+      const req = this.http.post<Post>(this.path, data)
+      .pipe(map(data => "I am " + data.title))     
       .subscribe(
         data => console.log(data),
          err => console.log("Error Occured!")
@@ -44,3 +48,4 @@ export class GetrequestComponent implements OnInit {
     }
 
 }
+
---
+++ src/app/post.ts
@@ -0,0 +1,6 @@
+// used in getrequest component for post req
+export interface Post {    
+    title: string,
+    body: string,
+    userId: number  
+}
