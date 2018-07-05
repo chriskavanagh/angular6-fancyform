1bcd7b021f8629d268c6580ec6516d6cb80ed207
Chris Kavanagh <ckava3@gmail.com>
2018-07-05 05:08:01 -0400

    fancy in fire component

--- src/app/fire/fire.component.ts
+++ src/app/fire/fire.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 
 // interface for collection from firestore db.
@@ -19,13 +20,18 @@ interface Users {
   styleUrls: ['./fire.component.css']
 })
 export class FireComponent implements OnInit {
-
+  
+  showComponent: boolean = true;
   userscollection: AngularFirestoreCollection<Users>;
   users: Observable<Users[]>;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, private _router: Router) { }
 
   ngOnInit() {
+    if(this._router.url === '/fire') {
+      this.showComponent === true;
+    }
+
     //console.log(this.ref);
     this.userscollection = this.afs.collection('users')
     /* this.userscollection = this.afs.collection('users', ref => {
