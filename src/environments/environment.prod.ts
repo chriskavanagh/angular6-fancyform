3f299c54a2a14c91a1453b761eef795f66e5d055
Chris Kavanagh <ckava3@gmail.com>
2018-07-13 04:17:44 -0400

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBXO3tAMcRPbWU6vvJwisDdydThJIrvkRU',
+    authDomain: 'frontend-b04f9.firebaseapp.com',
+    databaseURL: 'https://frontend-b04f9.firebaseio.com',
+    projectId: 'frontend-b04f9',
+    storageBucket: 'frontend-b04f9.appspot.com',
+    messagingSenderId: '805413240622'
+  }
 };
